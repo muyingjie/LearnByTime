224d753c806bbc2ec4ee1680400ca3da2f32e68c
myj-home <1368468125@qq.com>
2016-12-17 15:52:40 +0800

    删除PHPstorm自动生成的idea目录

---
+++ js/DataServices/DeepIntoPHP.js
@@ -0,0 +1,18 @@
+lbt.service("DeepIntoPHP",[function(){
+	return {
+		name:"PHP",
+		chapters:[{
+			name:"PHP概述及开发环境",
+			content:{
+				dirName:"DeepIntoPHP",
+				noteList:[{
+					title:"PHP概述",
+					items:[{
+						showStyle:"txt",
+						specContent:""
+					}]
+				}]
+			}
+		}]
+	};
+}]);
